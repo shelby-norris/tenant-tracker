e42e4df9365de848d7a788be8ac4ba6271b081f3
Shelby Norris <shelbs9109@gmail.com>
2025-09-09 11:21:08 -0500

    created new tenant intake form, created state, added submit button, added all tenants section to record each tenant's information, ensured Add Tenant button would add the tenants to the All Tenant Section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -16,24 +16,75 @@ import {
 // utilitiesIncluded: boolean
 
 export default function Main() {
+  const [tenants, setTenants] = useState([]);
+
+  const handleAddTenant = (event) => {
+    event.preventDefault();
+
+    const newTenant = {};
+
+    newTenant.name = event.target.tenantName.value;
+    newTenant.phone = parseInt (event.target.tenantPhone.value);
+    newTenant.property = event.target.propertyLeased.value;
+    newTenant.utilitiesIncluded = event.target.utilitiesIncluded.checked;
+
+    console.log(newTenant)
+
+    setTenants((oldTenants) => {
+     return [...oldTenants, newTenant]
+    })
+  };
+
   return (
     <main>
-      <form action="">
+      <h2>New Tenant Intake Form</h2>
+      <form onSubmit={(event) => handleAddTenant(event)}>
         <label htmlFor="">Name</label>
-        <input type="text" name="" id="" />
+        <input type="text" name="tenantName" id="tenantName" />
         <br />
 
         <label htmlFor="">Phone Number</label>
-        <input type="number" name="" id="" />
+        <input type="number" name="tenantPhone" id="tenantPhone" />
         <br />
 
         <label htmlFor="">Property Leased</label>
-        <input type="text" name="" id="" />
+        <input type="text" name="propertyLeased" id="propertyLeased" />
         <br />
 
         <label htmlFor="">Utilities Included</label>
-        <input type="checkbox" name="" id="" />
+        <input
+          type="checkbox"
+          name="utilitiesIncluded"
+          id="utilitiesIncluded"
+        />
+        <br />
+
+        <button type="submit">Add Tenant</button>
       </form>
+
+      <section>
+        <h2>All Tenants</h2>
+        {tenants.length == 0 ? (
+          <p>No Current Tenants</p> // If no tenants in database, p will display
+        ) : (
+          <div>
+            {tenants && tenants.map((tenantObject, index) => {
+              return (
+                <div className="all-tenants" key={index}>
+                  <p>{tenantObject.name}</p>
+                  <p>{tenantObject.phone}</p>
+                  <p>{tenantObject.property}</p>
+                  <p>
+                    {tenantObject.utilitiesIncluded
+                      ? "Utilities Included"
+                      : "Utilities Not Included"}
+                  </p>
+                </div>
+              );
+            })}
+          </div> // If tenants are greater than 0, div will display them all
+        )}
+      </section>
     </main>
   );
 }
