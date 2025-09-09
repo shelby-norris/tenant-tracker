55f23a22eb3d884d26afef7fbb350eedada3e933
Shelby Norris <shelbs9109@gmail.com>
2025-09-08 20:50:30 -0500

    deleted unneeded css file, added utils folder for dynamodb commands and tests, installed vitest and edited config and json files to accommodate testing, created components folder for Main header and footer, exported those into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.scss";
+import Header from "./components/Header.jsx";
+import Main from "./components/Main.jsx";
+import Footer from "./components/Footer.jsx";
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
- 
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
