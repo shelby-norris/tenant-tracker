a8ded0bceaa84510e1591cc19671fd289d7ccc35
Shelby Norris <shelbs9109@gmail.com>
2025-09-09 13:08:08 -0500

    used DynamoDB helpers to add new tenant to tenant database in DynamoDB table with useEffect and useState (setTenants)

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
 import {
-  // listAllItems,
   createItem,
   updateItem,
   deleteItem,
+  listAllItems,
 } from "../utils/dynamo";
 
 // Main component exported to App.jsx for more organized code
@@ -18,21 +18,33 @@ import {
 export default function Main() {
   const [tenants, setTenants] = useState([]);
 
-  const handleAddTenant = (event) => {
+  useEffect(() => {
+    (async () => {
+      const items = await listAllItems("Tenants");
+      console.log(items);
+
+      setTenants(items);
+    })();
+  }, []);
+
+  const handleAddTenant = async (event) => {
     event.preventDefault();
 
     const newTenant = {};
 
+    newTenant.id = Date.now().toString(); // unique identifier
     newTenant.name = event.target.tenantName.value;
-    newTenant.phone = parseInt (event.target.tenantPhone.value);
+    newTenant.phone = parseInt(event.target.tenantPhone.value);
     newTenant.property = event.target.propertyLeased.value;
     newTenant.utilitiesIncluded = event.target.utilitiesIncluded.checked;
 
-    console.log(newTenant)
+    console.log(newTenant);
+
+    await createItem("Tenants", newTenant);
 
     setTenants((oldTenants) => {
-     return [...oldTenants, newTenant]
-    })
+      return [...oldTenants, newTenant];
+    });
   };
 
   return (
@@ -68,20 +80,21 @@ export default function Main() {
           <p>No Current Tenants</p> // If no tenants in database, p will display
         ) : (
           <div>
-            {tenants && tenants.map((tenantObject, index) => {
-              return (
-                <div className="all-tenants" key={index}>
-                  <p>{tenantObject.name}</p>
-                  <p>{tenantObject.phone}</p>
-                  <p>{tenantObject.property}</p>
-                  <p>
-                    {tenantObject.utilitiesIncluded
-                      ? "Utilities Included"
-                      : "Utilities Not Included"}
-                  </p>
-                </div>
-              );
-            })}
+            {tenants &&
+              tenants.map((tenantObject, index) => {
+                return (
+                  <div key={index}>
+                    <p>{tenantObject.name}</p>
+                    <p>{tenantObject.phone}</p>
+                    <p>{tenantObject.property}</p>
+                    <p>
+                      {tenantObject.utilitiesIncluded
+                        ? "Utilities Included"
+                        : "Utilities Not Included"}
+                    </p>
+                  </div>
+                );
+              })}
           </div> // If tenants are greater than 0, div will display them all
         )}
       </section>
